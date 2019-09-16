504a088f7f12d7b94b570129f3dd61bc6026d62a
FamousMyung <famousmyung@gmail.com>
2019-09-17 04:09:00 +0900

    스플래쉬 화면 구현완료

--- app/src/main/java/com/stac/otk_oth_application/MainActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/MainActivity.kt
@@ -2,11 +2,13 @@ package com.stac.otk_oth_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("메인화면 입니다~")
     }
 }
--- app/src/main/java/com/stac/otk_oth_application/SplashActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.stac.otk_oth_application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity<MainActivity>()
+            finish()
+        }, 2000)
     }
 }
