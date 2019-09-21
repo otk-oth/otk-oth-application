72bd390c04f106cde6f17bf7fda7fefbdedaa6c2
B1ns <qlsl3574@naver.com>
2019-09-21 21:15:38 +0900

    [Update] Login UI 페이지

--- app/src/main/java/com/stac/otk_oth_application/view/login/LoginActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/view/login/LoginActivity.kt
@@ -3,11 +3,21 @@ package com.stac.otk_oth_application.view.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.stac.otk_oth_application.R
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonListener()
+    }
+
+    private fun buttonListener() {
+        register_text.setOnClickListener {
+            startActivity<RegisterActivity>()
+        }
     }
 }
