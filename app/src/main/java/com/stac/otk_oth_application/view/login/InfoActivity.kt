07e894733f1e2df31faea99ac2b59d985be2bdb7
B1ns <qlsl3574@naver.com>
2019-09-22 15:40:22 +0900

    [add] Firebase Lib.. { Database, FireStore, Storage }

--- app/src/main/java/com/stac/otk_oth_application/view/login/InfoActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/view/login/InfoActivity.kt
@@ -6,6 +6,8 @@ import com.stac.otk_oth_application.R
 
 class InfoActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
