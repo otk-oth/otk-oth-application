2e3c96eab665ec51992bd3208def6b7581498b2c
B1ns <qlsl3574@naver.com>
2019-09-22 10:17:35 +0900

    [add] 구글 로그인 완성

--- app/src/main/java/com/stac/otk_oth_application/view/login/LoginActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/view/login/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.stac.otk_oth_application.view.login
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.stac.otk_oth_application.R
 import com.stac.otk_oth_application.toast
 import com.stac.otk_oth_application.view.main.MainActivity
@@ -15,9 +26,11 @@ import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    var userEmail: String? = null
-    var userPw: String? = null
+    private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private var userEmail: String? = null
+    private var userPw: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +41,24 @@ class LoginActivity : AppCompatActivity() {
         login.setOnClickListener {
             email()
         }
+
+        googleAuth()
+
     }
+
+
+    private fun buttonListener() {
+        register_text.setOnClickListener {
+            startActivity<RegisterActivity>()
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(login_email.windowToken, 0)
+        return true
+    }
+
 
     private fun email() {
         userEmail = login_email.text.toString()
@@ -49,16 +79,77 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    private fun googleAuth() {
 
-    private fun buttonListener() {
-        register_text.setOnClickListener {
-            startActivity<RegisterActivity>()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        google.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+
+            }
         }
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(login_email.windowToken, 0)
-        return true
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    loginSucceed(user)
+                } else {
+                    toast("인증 실패")
+                    loginSucceed(null)
+                }
+            }
     }
+
+    private fun loginSucceed(user: FirebaseUser?) {
+        if (user != null) {
+            toast("로그인 성공 !")
+            startActivity<InfoActivity>()
+            finish()
+        }
+    }
+
+
+    override fun onBackPressed() {
+        val tempTime = System.currentTimeMillis()
+        val intervalTime = tempTime - backPressedTime
+        if (intervalTime in 0..FINSH_INTERVAL_TIME) {
+            ActivityCompat.finishAffinity(this)
+        } else {
+            backPressedTime = tempTime
+            toast("한번 더 누르시면 종료됩니다.")
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+        private val FINSH_INTERVAL_TIME = 2000
+        private var backPressedTime: Long = 0
+    }
+
 }
