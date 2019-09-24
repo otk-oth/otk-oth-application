cede80288f5fc3c7e9a74ccf1ae4d94d65048c75
B1ns <qlsl3574@naver.com>
2019-09-24 18:08:49 +0900

    [Update] Main - UI Finish

--- app/src/main/java/com/stac/otk_oth_application/view/main/MainActivity.kt
+++ app/src/main/java/com/stac/otk_oth_application/view/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.stac.otk_oth_application.view.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,12 @@ import com.stac.otk_oth_application.R
 import com.stac.otk_oth_application.view.map.MapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet.*
+import android.text.Spannable
+import com.stac.otk_oth_application.utiles.CustomTypefaceSpan
+import android.text.SpannableString
+import android.graphics.Typeface
+import androidx.core.view.GravityCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +25,76 @@ class MainActivity : AppCompatActivity() {
 
         setFragment()
         setBottomSheet()
+        setFontChange()
+        setButtonListener()
+
+    }
+
+    private fun setButtonListener() {
+
+        // 드로워 네비게이션 을 열어주는 메뉴 버튼을 눌렀을시 드로워 네비게이션을 열어줌.
+        main_menu.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        // 드로워 네비게이션 아이템 선택했을때 이벤트 발생 코드
+        when (item!!.itemId) {
+            // 마이페이지
+            R.id.myPage -> {
+
+            }
+            // 포인트샵
+            R.id.pointShop -> {
+
+            }
+            // 분실물
+            R.id.lost -> {
+
+            }
+
+            // 세팅
+            R.id.setting -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setFontChange() {
+        val menu = main_navigation.menu
+
+        for (i in 0 until menu.size()) {
+            val mi = menu.getItem(i)
+
+            // 폰트를 드로워 네비게이션에 적용하는 중
+            val subMenu = mi.subMenu
+            if (subMenu != null && subMenu.size() > 0) {
+                for (j in 0 until subMenu.size()) {
+                    val subMenuItem = subMenu.getItem(j)
+                    setFont(subMenuItem)
+                }
+            }
+
+            setFont(mi)
+        }
+    }
+
+    fun setFont(menuItem: MenuItem) {
+
+        // menuItem을 폰트 형태로 바꾸는 작업
+        val font = Typeface.createFromAsset(assets, "korean.otf")
+        val mNewTitle = SpannableString(menuItem.title)
+        mNewTitle.setSpan(
+            CustomTypefaceSpan("", font),
+            0,
+            mNewTitle.length,
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        menuItem.title = mNewTitle
     }
 
     private fun setFragment() {
@@ -94,4 +171,13 @@ class MainActivity : AppCompatActivity() {
 
             }
     }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
